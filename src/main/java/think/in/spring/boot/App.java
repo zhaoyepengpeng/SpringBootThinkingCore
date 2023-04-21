ed24f095f31ca96e1a2ca66465674534fac7c537
zhaoyepengpeng <1285872896@qq.com>
2023-04-21 14:11:09 +0800

    spring boot actuator 测试

--- src/main/java/think/in/spring/boot/App.java
+++ src/main/java/think/in/spring/boot/App.java
@@ -24,7 +24,7 @@ public class App {
      * 在页面访问/actuator   或者  /actuator/health  或者 /actuator/info 返回{} 由程序员定制（get方式）
      * http://127.0.0.1:8081/actuator/health 返回{"status":"UP"} 表示正常
      *
-     * 访问访问/actuator返回数据
+     * 访问访问/actuator,默认返回数据：
      * {
      * 	"_links": {
      * 		"self": {
@@ -40,6 +40,83 @@ public class App {
      * 			"templated": false
      *        }* 	}
      * }
+     * 要返回所有的则在配置文件中配置，返回数据为：
+     * {
+     * 	"_links": {
+     * 		"self": {
+     * 			"href": "http://127.0.0.1:8081/actuator",
+     * 			"templated": false
+     *                },
+     * 		"auditevents": {
+     * 			"href": "http://127.0.0.1:8081/actuator/auditevents",
+     * 			"templated": false
+     *        },
+     * 		"beans": {
+     * 			"href": "http://127.0.0.1:8081/actuator/beans",
+     * 			"templated": false
+     *        },
+     * 		"health": {
+     * 			"href": "http://127.0.0.1:8081/actuator/health",
+     * 			"templated": false
+     *        },
+     * 		"conditions": {
+     * 			"href": "http://127.0.0.1:8081/actuator/conditions",
+     * 			"templated": false
+     *        },
+     * 		"configprops": {
+     * 			"href": "http://127.0.0.1:8081/actuator/configprops",
+     * 			"templated": false
+     *        },
+     * 		"env": {
+     * 			"href": "http://127.0.0.1:8081/actuator/env",
+     * 			"templated": false
+     *        },
+     * 		"env-toMatch": {
+     * 			"href": "http://127.0.0.1:8081/actuator/env/{toMatch}",
+     * 			"templated": true
+     *        },
+     * 		"info": {
+     * 			"href": "http://127.0.0.1:8081/actuator/info",
+     * 			"templated": false
+     *        },
+     * 		"loggers": {
+     * 			"href": "http://127.0.0.1:8081/actuator/loggers",
+     * 			"templated": false
+     *        },
+     * 		"loggers-name": {
+     * 			"href": "http://127.0.0.1:8081/actuator/loggers/{name}",
+     * 			"templated": true
+     *        },
+     * 		"heapdump": {
+     * 			"href": "http://127.0.0.1:8081/actuator/heapdump",
+     * 			"templated": false
+     *        },
+     * 		"threaddump": {
+     * 			"href": "http://127.0.0.1:8081/actuator/threaddump",
+     * 			"templated": false
+     *        },
+     * 		"metrics": {
+     * 			"href": "http://127.0.0.1:8081/actuator/metrics",
+     * 			"templated": false
+     *        },
+     * 		"metrics-requiredMetricName": {
+     * 			"href": "http://127.0.0.1:8081/actuator/metrics/{requiredMetricName}",
+     * 			"templated": true
+     *        },
+     * 		"scheduledtasks": {
+     * 			"href": "http://127.0.0.1:8081/actuator/scheduledtasks",
+     * 			"templated": false
+     *        },
+     * 		"httptrace": {
+     * 			"href": "http://127.0.0.1:8081/actuator/httptrace",
+     * 			"templated": false
+     *        },
+     * 		"mappings": {
+     * 			"href": "http://127.0.0.1:8081/actuator/mappings",
+     * 			"templated": false
+     *        }* 	}
+     * }
+     *
      */
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
